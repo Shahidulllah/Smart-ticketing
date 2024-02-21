dafb2c4fe6bd184c91c7e1ad11f5779bb69c5556
shahidullah177 <mdshahidsumon177@gmail.com>
2024-02-21 19:12:13 +0600

    Almost complete

--- js/interactive.js
+++ js/interactive.js
@@ -3,17 +3,16 @@ const seats = document.querySelectorAll('#A1, #A2, #A3, #A4, #B1, #B2, #B3, #B4,
 let count = 0;
 for (const seat of seats) {
     seat.addEventListener('click', function () {
-
         if (count < 4) {
             this.style.backgroundColor = '#1DD100';
             this.style.color = 'white';
             count++;
-
+            
             // Seat Left function
             const seatLeft = document.getElementById('seatLeft');
             const seatElement = seatLeft.innerText;
-            const seat = parseInt(seatElement);
-            const updateSeat = seat - 1;
+            const convertSeat = parseInt(seatElement);
+            const updateSeat = convertSeat - 1;
             seatLeft.innerText = updateSeat;
 
 
@@ -38,26 +37,50 @@ for (const seat of seats) {
             const validTk = parseInt(grandPayment);
             grandTk.innerText = currnetTk;
         }
+       
         else {
             alert("It's not available to buy more than four tickets..")
         }
-
-
         
-       
+        // Append selected seat to the items
+      const selectedSeat = document.getElementById('selected-seat');
+      const div = document.createElement("div");
+      div.classList.add('flex', 'justify-between', 'mx-7');
+      const p1 = document.createElement("p");
+      const p2 = document.createElement("p");
+      const p3 = document.createElement("p");
+      
+
+        p1.innerText = seat.innerText;
+        p2.innerText = 'economy';
+        p3.innerText = '550'
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+
+        selectedSeat.appendChild(div);
+      
     })
 }
  // coupon 
- const btn = document.getElementById('applyBtn');
- btn.addEventListener('click', function(){
-    const a = document.getElementById('input').value;
-    if(a === 'NEW15' || a === 'Couple 20'){
-        grandTk.innerText = currnetTk - (currnetTk * 0.15);
+    function applyBtn(){
+        const inputt = document.getElementById('input');
+        const inputValue = inputt.value
+        if(inputValue === 'NEW15'){
+          const discount = grandTk.innerText * 0.15;
+          const grandTotal = grandTk.innerText - discount;
+          grandTk.innerText = grandTotal;
+        }
+        else if(inputValue === 'Couple 20'){
+            const discount = grandTk.innerText * 0.20;
+            const grandTotal = grandTk.innerText - discount;
+            grandTk.innerText = grandTotal;
+        }
+
+        else{
+            alert('Invalid coupon..!')
+        }
     }
-    else{
-        alert('Give a valid coupon')
-    }
- })
  
 
 
